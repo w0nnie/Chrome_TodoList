1c99aa5c29cb9d879c516c24ff916b87e7229cf1
w0nnie <kjw55523@gmail.com>
2022-02-13 21:41:21 +0900

    [feat] app.js [feat] index.html [feat] style.css #start_file_set

---
+++ app.js
@@ -0,0 +1,10 @@
+const a = 5;
+const b = "2";
+let myName = "jinwon";
+
+//let, const의 차이 
+//const 상수 선언해준 값을 변경할 수 없다.
+//const 값을 선언하고 절대 바꾸지 않아야 할때 사용하면 될듯
+console.log(a + 2);
+console.log(a+b);
+console.log("hello"+ myName);
