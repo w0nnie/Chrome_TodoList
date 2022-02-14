812573c149d0308b32f89ad73e0c8e56a47f056c
w0nnie <kjw55523@gmail.com>
2022-02-14 22:53:17 +0900

    #2.8 Functions

--- app.js
+++ app.js
@@ -7,4 +7,15 @@ let myName = "jinwon";
 //const 값을 선언하고 절대 바꾸지 않아야 할때 사용하면 될듯
 console.log(a + 2);
 console.log(a+b);
-console.log("hello"+ myName);
+console.log("hello "+ myName);
+
+//object와 function의 매겨변수 보내기
+const player = {
+    name:"jinwon",
+    sayHello: function (otherPersonName){
+        console.log("Hello " + otherPersonName + " nice to meet u!");
+    }
+};
+
+player.sayHello("jieun");
+player.sayHello("haha");
