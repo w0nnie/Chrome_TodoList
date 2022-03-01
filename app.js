b4c6960b9e8f798bfb069495a26e21cce91b2943
jinwonkwon <kjw55523@gmail.com>
2022-03-01 23:17:19 +0900

    #4.4 Getting Username

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden" //upper case 별 의미없는 정보를 담는 변수일 때 사용
 const link = document.querySelector("a");
 
-// #4.3 Events part Two 
+// #4.4 Getting Username
 
-function Click(){
+function Click(event){
+        event.preventDefault();
         const username = loginInput.value;
+        loginForm.classList.add(HIDDEN_CLASSNAME);
+        console.log(username);
+        greeting.innerText= "Hello " + username;
+        greeting.innerText= `Hello ${username}`;
+        greeting.classList.remove(HIDDEN_CLASSNAME);
+ 
         console.log(username);
 }
 
