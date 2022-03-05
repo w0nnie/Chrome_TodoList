ce07dff3000891741d44e96a07f68a1be114394d
jinwonkwon <kjw55523@gmail.com>
2022-03-05 23:28:43 +0900

    #4.7 Super Recap

--- app.js
+++ app.js
@@ -2,21 +2,15 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden" //upper case 별 의미없는 정보를 담는 변수일 때 사용
+const USERNAME_KEY = "username";
 const link = document.querySelector("a");
 
-// #4.5 Saving Username
+// #4.7 Super Recap 
 
-function Click(event){
-        event.preventDefault();sss
+function onLoginSubmit(event){
         const username = loginInput.value;
-        const username1 = "hi";
         loginForm.classList.add(HIDDEN_CLASSNAME);
-        localStorage.setItem("username",username);
-        greeting.innerText= "Hello " + username;
-        greeting.innerText= `Hello ${username}`;
-        greeting.classList.remove(HIDDEN_CLASSNAME);
- 
-        console.log(username);
+        localStorage.setItem(USERNAME_KEY,username);
 }
 
 function handleLinckClick(event){
@@ -25,5 +19,14 @@ function handleLinckClick(event){
         alert("clicked!");
 }
 
-loginForm.addEventListener("submit",Click); 
+const savedUsername = localStorage.getItem(USERNAME_KEY); //localStorage 값이 있는지 체크해주기 위한 인자값 생성
+
+if (savedUsername == null){ //localStorage 값 체크
+        loginForm.classList.remove(HIDDEN_CLASSNAME);
+        loginForm.addEventListener("submit",onLoginSubmit); 
+}else{
+        greeting.innerText= `Hello ${savedUsername}`;
+        greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
 link.addEventListener("click",handleLinckClick);
