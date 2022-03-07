7d72aa9d8ab52bbbb93ac22fa6d0023f7c49f9a9
jinwonkwon <kjw55523@gmail.com>
2022-03-07 23:12:59 +0900

    #7.1 Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+// #7.1 Adding ToDos 
+function printTodo(hi){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = hi;
+    toDoList.appendChild(li);
+}
+
 function handleTodoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    printTodo(newTodo);
 }
 toDoForm.addEventListener("submit",handleTodoSubmit);
