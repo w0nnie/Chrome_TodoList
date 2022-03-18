d63da292d630575cb16ce518205ec06da7885c3f
kjw <kjw55523@gmail.com>
2022-03-18 13:08:27 +0900

    #8.1 Weather API SET weather API

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,11 @@
+const API_KEY = "a19e83f0166783d213dbabb84ed2dc69";
 //#8.0 Geolocation
 function onGeoOk(position){
         const lat = position.coords.latitude;
         const long = position.coords.longitude;
         console.log("You live in",lat,long);
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`;
+        console.log(url);
 }
 function onGeoError(){
         alert("Can't find you. No weather for you.");
